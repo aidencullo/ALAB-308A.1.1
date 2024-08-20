aaeb52aa1b36423cb9b9f5887b8c532ad37217b5
Aiden Cullo <aidencullo@gmail.com>
2024-08-20 12:07:42 -0400

    create main file

---
+++ main.js
@@ -0,0 +1,4 @@
+// Declare a global counter variable.
+// Create a simple function that increments the variable, and then calls itself recursively.
+// Surround the initial function call in a try/catch block.
+// Within the catch, log the error and the value of the counter variable.
