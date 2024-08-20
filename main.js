0ce04071395ba8f3329cd202d73c40418c3048d3
Aiden Cullo <aidencullo@gmail.com>
2024-08-20 12:09:19 -0400

    part 1

--- main.js
+++ main.js
@@ -2,3 +2,16 @@
 // Create a simple function that increments the variable, and then calls itself recursively.
 // Surround the initial function call in a try/catch block.
 // Within the catch, log the error and the value of the counter variable.
+
+let counter = 0;
+const increment = () => {
+  counter++;
+  increment();
+};
+
+try {
+	increment();
+} catch (error) {
+	console.log(`Error: ${error}`);
+	console.log(`Counter: ${counter}`);
+}
