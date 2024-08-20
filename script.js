2899e311a1a0a2d5997c83b77d6f502163e115f7
Aiden Cullo <aidencullo@gmail.com>
2024-08-20 12:25:15 -0400

    deferred execution

--- script.js
+++ script.js
@@ -24,9 +24,30 @@ try {
 // Using setTimeout, you can place tasks into the event queue. This sets the task to be executed after the current call stack is cleared and after the browser has had a moment to render.
 // Implement the following:
 // Create a simple HTML element to hold text. Cache this HTML element into a JavaScript variable.
-
+const element = document.createElement('div');
+document.body.appendChild(element);
 
 
 // Write a function that takes a parameter n and adds a list of all prime numbers between one and n to your HTML element.
-// Once complete, use the alert() method to alert the user that the calculation is finished.
+function addPrime(n){
+  for (let i = 2; i <= n; i++) {
+    if (isPrime(i)){
+      const el = document.createElement('div')
+      el.textContent = i;
+      element.appendChild(el);
+    }
+  }
+  alert('Calculation is finished');
+}
 
+function isPrime(n){
+  for (let i = 2; i < n; i++) {
+    if (n % i == 0){
+      return false;
+    }
+  }
+  return true;
+}
+
+addPrime(1000);
+// Once complete, use the alert() method to alert the user that the calculation is finished.
